1fb04daf279c050ad2a697288dcdaaa1c0e09e3f
Lars Gimse <lagi@iktsenteret.no>
2017-05-01 14:04:54 +0200

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,4 @@
+consumer_key        = 'RX4jw4alRLpGZgcBC01g7ZwPl'
+consumer_secret     = 'Dhc8s7tr96yGv1x62KdKWko3BlL273DNkwOxFqBY3CpDHuK8Wf'
+access_token        = '855340273580113920-MtTzqxZWnICDmhF2u38nuaHnOTFVbfZ'
+access_token_secret = 'PrY5Fz0PisWgLOzBl1Bk0KSIyYr0DHsOqiNggKXdNW4JY' 
